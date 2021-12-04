e8269bf29f835263dd21bdd01bfb4f6e622b8e05
Mingmar Dolma <62723387+mingmardolma@users.noreply.github.com>
2021-12-03 21:27:51 -0500

    Add files via upload

---
+++ Z Business/tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+total_households = psql.sqldf(q_total_households)
+# total_households = psql.sqldf(q_total_households)
+# total_households = psql.sqldf(q_total_households)
+# total_households = psql.sqldf(q_total_households)
+# total_households = psql.sqldf(q_total_households)
+# total_households = psql.sqldf(q_total_households)
